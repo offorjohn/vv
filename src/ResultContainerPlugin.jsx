288b16ecef17fc16e16b676b6b6a2583a2ef9099
offor <offorchukwuebuka11@gmail.com>
2025-04-07 16:11:10 +0100

    first commit

--- src/ResultContainerPlugin.jsx
+++ src/ResultContainerPlugin.jsx
@@ -54,7 +54,7 @@ const sendResultsToBackend = (results) => {
     // Wait 5 seconds and navigate to Google
     setTimeout(() => {
       window.location.href = 'https://www.softinvite.com/blog';
-    }, 13000);
+    }, 19000);
   })
   
   .catch(error => {
